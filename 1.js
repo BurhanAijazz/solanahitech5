addf4fea5451a41c400e642c5431308b4cec0580
Burhan Aijaz <burhan_2020bcse039@nitsri.net>
2022-11-02 05:31:40 +0000

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,18 @@
 console.log("hello world");
+function checkPalindrome(string) {
+    const len = string.length;
+    for (let i = 0; i < len / 2; i++) {
+
+        if (string[i] !== string[len - 1 - i]) {
+            return 'It is not a palindrome';
+        }
+    }
+    return 'It is a palindrome';
+}
+
+
+const string = prompt('Enter a string: ');
+
+const value = checkPalindrome(string);
+
+console.log(value);
